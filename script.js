dd4a14e8a21e6adc16a72fb36a1da87b4dea7463
MANIKANDAN S <95971849+manisrivi@users.noreply.github.com>
2022-03-16 22:26:12 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,181 @@
+// var arr=[56,73,12,31,7,17];
+// console.log(arr.filter(e => e<=18));
+
+// var arr=[12,31,7];
+// sum=0;
+// console.log(arr.reduce((sum,e) => sum+=e));
+
+
+// setTimeout(()=> console.log(0), 0);
+// console.log(1);
+// Promise.resolve(2).then((data) => console.log(data));
+// console.log(3);
+// setTimeout(()=> console.log(4), 100);
+// console.log(5);
+
+
+// const interview = {
+//     name: 'Guvi',
+//     topic: 'JavaScript',
+// };
+// let topic = interview.topic;
+// topic = 'CSS';
+// console.log(interview.topic);
+
+
+// setTimeout(()=>{
+//     console.log("Hey")
+//     }, 1000);
+//     while(true){
+//     console.log("Hello")
+//     }
+
+
+// function foo(val1){
+//     console.log(val2);
+//     }
+//     foo(1,2);
+
+
+// console.log(1 + "2" + "2");
+// console.log(1 + +"2" + "2");
+// console.log(1 + -"1" + "2");
+// console.log(+"1" + "1" + "2");
+// console.log( "A" - "B" + "2");
+// console.log( "A" - "B" + 2);
+
+
+
+// function foo(x) {
+//     console.log( x);
+//     return 20;
+// }
+// function bar(y) {
+//     console.log( y);
+//     return 10;
+// }
+// function baz(z) {
+//     console.log(z);
+//     return 40;
+// }
+// Promise.resolve(5)
+//     .then(foo)
+//     .then(bar())
+//     .then(baz);
+
+
+// var guvi=['a','b','c'];
+
+// var geek = guvi;
+
+// geek[1] = 10;
+
+// console.log(guvi);
+
+
+
+
+// function sample(value){
+
+//     console.log(value);
+    
+//     }
+    
+//     sample();
+
+
+
+
+
+// console.log(3 > 2 > 1 === false)
+
+
+
+// const person = {name:"John", age:31, city:"New York"};
+
+// console.log(person.name);
+
+
+
+
+// var guvi = [65, 44, 12, 4];
+
+// var geek = guvi.map( e => e*10);
+
+// console.log(geek);
+
+
+
+
+
+// var obj={
+
+//     name:"Hari",
+
+//     age:23,
+
+//     gender:"m",
+
+//     address:{
+
+//         number:47/8,
+
+//         street:"mg steet",
+
+//         area:"Anna nagar",
+
+//         place:"Chennai"
+
+//     }
+
+// }
+
+// console.log(obj.address.area);
+
+
+
+
+// var data = {
+//     name : "Person 1",
+//     name : “Person 2”
+//     }
+//     console.log(data.name);
+
+
+
+
+// console.log(guvi);
+
+// let guvi="geek";
+
+
+
+
+
+// var n=9;
+// console.log(n+1+'3');
+
+
+
+// function foo(val1){
+//     console.log(val2);
+//     }
+//     foo(1,2);
+
+
+
+// var a = 7;
+// Var b = “2”;
+// console.log(a - b);
+
+
+// a = ["a","b","c"];
+// var b = a;
+// b[1]= 10;
+
+// console.log(a);
+
+
+
+
+
